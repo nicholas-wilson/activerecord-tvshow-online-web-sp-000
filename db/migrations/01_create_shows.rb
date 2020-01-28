a37bd312c08f9e636759d7cf76de7062c3d04d5e
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 20:36:27 +0000

    Automatically backed up by Learn

--- db/migrations/01_create_shows.rb
+++ db/migrations/01_create_shows.rb
@@ -2,6 +2,9 @@ class CreateShows < ActiveRecord::Migration[5.1]
   def change
     create_table :shows do |s|
       s.string :name
+      s.string :day
+      s.string :network
+      s.integer :rating
     end
   end
 end
