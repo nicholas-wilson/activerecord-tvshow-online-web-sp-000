d15525093acb17eaf44246d11bc2703ae6854a7d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 21:10:44 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,8 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     Show.all.order(rating: :desc).first.rating
   end
+
+  def self.most_popular_show
+    Show.all.order(rating: :desc).first
+  end
 end
